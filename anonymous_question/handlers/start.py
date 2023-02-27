4eb86f15e73f3edc39c5e9d4d4e5402dfca929a2
Ivan <itokariuk.ua@gmail.com>
2023-02-27 14:39:21 +0200

    Fixed start handler

--- anonymous_question/handlers/start.py
+++ anonymous_question/handlers/start.py
@@ -1,4 +1,5 @@
 from aiogram import Router
+from aiogram.filters import CommandStart
 from aiogram.types import Message
 
 from anonymous_question.services.templates import render_template
@@ -7,5 +8,6 @@ from anonymous_question.services.templates import render_template
 start_router = Router()
 
 
+@start_router.message(CommandStart)
 async def start(message: Message):
     await message.answer(render_template("start.j2"))
